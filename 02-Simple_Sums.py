616a8f01960f40f6cb15b9cefd948fe932ff18bd
Saez-aly <saezaly0414@gmail.com>
2024-10-12 20:17:51 +0400

    Update 02-Simple_Sums.py

--- 02-Simple_Sums.py
+++ 02-Simple_Sums.py
@@ -3,11 +3,13 @@
 A1 Programming Skills Portfolio
 
 Exercise 2- Simple Sums
-Direction: In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+Direction: In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console. 
+The first number to be 8 and second number to be 10.
+
 
 """
 
 num1=8 #first int variable
 num2=10 #second int variable
-result=(num1+num2) #result stores the sum of the first two numbers.
+result=(num1+num2) #result stores the sum of the two numbers.
 print(result) #displays the sum
