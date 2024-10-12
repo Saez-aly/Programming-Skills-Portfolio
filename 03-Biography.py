69edab1f12b731140aa8d07db413d3bdd0498840
Saez-aly <saezaly0414@gmail.com>
2024-10-12 20:36:52 +0400

    Update 03-Biography.py

--- 03-Biography.py
+++ 03-Biography.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 """
 Exercise 3: Biography
 In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
 """
-# First step, getting the user input and storing it in variable as name, hometown, and age.
+# First step, getting the user's input and storing it in variable as name, hometown, and age.
 name = input("Please enter your full name ? ")  # string input for name
 hometown = input("Please enter your hometown ? ")  # string input for hometown
 age = input("Please your age ? ")  # string input for age
 
-#To ensure valid input for age,  we need to change the age_input from string to integer. Age is a number. 
+#To ensure valid input for age,  we need to change the age from string to integer. Age is a number. 
 
 # Second step: Validating age input
 if age.isdigit():  # checks if the age input is a valid integer (digits only)
@@ -25,7 +24,7 @@ user_info = {
 
 # Finally, the last step; displays the user information, and checks if age input is valid.
 if user_info["age"] is not None:
-    print(f"Name: {user_info['name']}\nHometown: {user_info['hometown']}\nAge: {user_info['age']}")
+    print("Name: " + user_info["name"] + "\nHometown: " + user_info["hometown"] + "\nAge: " + str(user_info["age"]))
 else:
-    print(f"Name: {user_info['name']}\nHometown: {user_info['hometown']}\nAge: Invalid")
+    print("Name: " + user_info["name"] + "\nHometown: " + user_info["hometown"] + "\nAge: Invalid")
 
