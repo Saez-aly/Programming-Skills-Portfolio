bd78e570b2c92e8a9a9988bdaf8eba37facd823c
Saez-aly <saezaly0414@gmail.com>
2024-11-07 10:40:11 +0400

    Update 01-Coding_is_Cool.py

--- 01-Coding_is_Cool.py
+++ 01-Coding_is_Cool.py
@@ -16,3 +16,7 @@ word3 = "cool"   #third word
 #However, the output should be 'Coding is cool'
 
 print(word1, word2, word3) # I have researched online that this action is the easiet method to add spaces between the words, which displays 'Coding is Cool'.
+
+"""what I have learned: In this simple activity, I have learned about string concatenation in Python. 
+Also I need to be mindful about adding space for output readability.
+Overall, I gained understanding the difference between direct concatenating string and refining code yet also meeting the required output."""
