0ad4d499fd030b34b3075d11447eed38d4977f1e
Saez-aly <saezaly0414@gmail.com>
2024-11-07 10:58:15 +0400

    Update 09-Hello.py

--- 09-Hello.py
+++ 09-Hello.py
@@ -14,3 +14,6 @@ if __name__ == "__main__":
 
 #Before ending this code I also want to share the greeting function I have learned in 'sololearn' which was designed to greet, or say hellow whenever called. 
 #I have not incorporated in this code since is 'fill_in_the_blank'.
+
+""" what i have learned: ello I remember learning the greeting function in solo learn, but upon completing this activity, I realized that this was fill in the blanks. Here I learned to recognize the importance of reusable function,
+and structuring a python script to of course enhance to code modularity while also understandable by incorporationg command line."""
